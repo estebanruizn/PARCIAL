a367cdc3d8e7f8cc61b1a31093995f5362faf6b9
Esteban Ruiz Novoa <esteban.ruiz@uniminuto.edu.co>
2023-03-14 19:23:20 -0500

    ESTEBAN ANDRES RUIZ NOVOA

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,7 +11,7 @@ fun main(args: Array<String>) {
         println("2. Contar monedas")
         println("3. Calcular dinero ahorrado")
         println("4. Romper alcancia")
-        println("5. Comenzar nueva alcancia")
+        println("5. Comenzar una nueva alcancia")
         println("Ingrese una opcion")
         var opc = readln().toInt()
         if (opc==1){
